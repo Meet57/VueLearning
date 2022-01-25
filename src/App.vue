952217544809fb4bb9a30f066798fd7c124b9190
meet57 <meet57.mp@gmail.com>
2022-01-26 01:35:46 +0530

    day 1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <Ninjas />
+  </div>
+</template>
+
+<script>
+import Ninjas from "./components/Ninjas.vue";
+export default {
+  components: { Ninjas },
+  data() {
+    return {
+      title: "Ninja App",
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: purple;
+}
+</style>
---
+++ src/components/Ninjas.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>List of ninjas</h1>
+    <ul>
+      <li v-for="(item, index) in ninjas" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Ninjas",
+  data() {
+    return {
+      ninjas: ["Meet", "Parth", "Rishi"],
+    };
+  },
+};
+</script>
+
+<style scoped>
+    h1{
+        color: green;
+    }
+</style>
