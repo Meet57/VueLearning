dea0e8fce67b2963b5d442b7f97fa9844ce60b36
meet57 <meet57.mp@gmail.com>
2022-01-27 13:43:58 +0530

    check boxes

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -7,12 +7,27 @@
             <input type="text" v-model.lazy="blog.title" />
             <label>Blog Content:</label>
             <textarea v-model.lazy.trim="blog.content"></textarea>
+            <div id="checkboxes">
+                <p>Blog Categories:</p>
+                <label>Ninjas</label>
+                <input type="checkbox" value="ninjas" v-model="blog.categories" />
+                <label>Wizards</label>
+                <input type="checkbox" value="wizards" v-model="blog.categories" />
+                <label>Mario</label>
+                <input type="checkbox" value="mario" v-model="blog.categories" />
+                <label>Cheese</label>
+                <input type="checkbox" value="cheese" v-model="blog.categories" />
+            </div>
         </form>
         <div id="preview">
             <h3>Preview blog</h3>
             <p>Blog title: {{ blog.title }}</p>
             <p>Blog content:</p>
             <p style="white-space: pre">{{ blog.content }}</p>
+            <p>Blog Categories:</p>
+            <ul>
+                <li v-for="cat in blog.categories" :key="cat">{{cat}}</li>
+            </ul>
         </div>
     </div>
 </template>
@@ -24,6 +39,7 @@ export default {
             blog: {
                 title: "",
                 content: "",
+                categories: []
             },
         };
     },
@@ -32,29 +48,35 @@ export default {
 </script>
 
 <style>
-#add-blog * {
+#add-blog *{
     box-sizing: border-box;
 }
-#add-blog {
+#add-blog{
     margin: 20px auto;
     max-width: 500px;
 }
-label {
+label{
     display: block;
     margin: 20px 0 10px;
 }
-input[type="text"],
-textarea {
+input[type="text"], textarea{
     display: block;
     width: 100%;
     padding: 8px;
 }
-#preview {
+#preview{
     padding: 10px 20px;
     border: 1px dotted #ccc;
     margin: 30px 0;
 }
-h3 {
+h3{
     margin-top: 10px;
+}
+#checkboxes input{
+    display: inline-block;
+    margin-right: 10px;
+}
+#checkboxes label{
+    display: inline-block;
 }
 </style>
