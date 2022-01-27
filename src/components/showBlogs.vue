e58b876c074f20efca600e2ba2f6bbd2dcd9b6b4
meet57 <meet57.mp@gmail.com>
2022-01-27 17:03:10 +0530

    local directives and filters

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,10 +1,10 @@
 <template>
     <div v-theme:column="theme" id="show-blogs">
         <h1>All Blogs Articles</h1>
-        <input type="text" v-model="search" placeholder="Search Box">
+        <input type="text" v-model="search" placeholder="Search Box" />
         <h3 v-if="isLoading">Loading...</h3>
         <div v-else v-for="blog in filteredBlogs" :key="blog.id" class="single-blog">
-            <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+            <h2 v-rainbow>{{ blog.title | toUppercase }}</h2>
             <p>{{ blog.body | snippet }}</p>
         </div>
     </div>
@@ -15,25 +15,54 @@ export default {
     data() {
         return {
             blogs: [],
-            isLoading : true,
-            theme: 'narrow',
-            search: ''
+            isLoading: true,
+            theme: "narrow",
+            search: "",
         };
     },
     methods: {},
     created() {
         this.$http.get("https://jsonplaceholder.typicode.com/posts").then((data) => {
             this.blogs = data.body.slice(0, 10);
-            this.isLoading = false
+            this.isLoading = false;
         });
     },
-    computed:{
-        filteredBlogs(){
+    computed: {
+        filteredBlogs() {
             return this.blogs.filter((blog) => {
-                return blog.title.toLowerCase().match(this.search.toLowerCase())
-            })
-        }
-    }
+                return blog.title.toLowerCase().match(this.search.toLowerCase());
+            });
+        },
+    },
+    filters: {
+        toUppercase(value) {
+            return value.toUpperCase();
+        },
+
+        snippet(value) {
+            return value.length + " - " + value.slice(0, 200) + "...";
+        },
+    },
+    directives: {
+        rainbow: {
+            bind(el) {
+                el.style.color = "#" + Math.random().toString().slice(2, 8);
+            },
+        },
+        theme: {
+            bind(el, binding) {
+                if (binding.value == "wide") {
+                    el.style.maxWidth = "1200px";
+                } else if (binding.value == "narrow") {
+                    el.style.maxWidth = "560px";
+                }
+                if (binding.arg == "column") {
+                    el.style.background = "#ddd";
+                    el.style.padding = "20px";
+                }
+            },
+        },
+    },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -7,35 +7,8 @@ Vue.config.productionTip = false;
 export const bus = new Vue();
 
 // custom directives
-Vue.directive("rainbow", {
-    bind(el) {
-        el.style.color = "#" + Math.random().toString().slice(2, 8);
-    },
-});
-
-Vue.directive("theme", {
-    bind(el, binding) {
-        if (binding.value == "wide") {
-            el.style.maxWidth = "1200px";
-        } else if (binding.value == "narrow") {
-            el.style.maxWidth = "560px";
-        }
-        if (binding.arg == "column") {
-            el.style.background = "#ddd";
-            el.style.padding = "20px";
-        }
-    },
-});
 
 //Filters
-
-Vue.filter("to-uppercase", (value) => {
-    return value.toUpperCase();
-});
-
-Vue.filter("snippet", (value) => {
-    return value.slice(100) + '...'
-});
 
 Vue.use(VueResource);
 
