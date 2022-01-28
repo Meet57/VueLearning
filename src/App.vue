1846e9ae87b556c28ef4b14e1396003b41d631c6
meet57 <meet57.mp@gmail.com>
2022-01-28 15:06:42 +0530

    route links

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-    <div>
+    <div id="body">
+        <app-header></app-header>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
 export default {
     components:{
+        'app-header':Header
     },
     data(){
         return {
@@ -19,4 +22,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+#body{
+    margin: 0;
+    padding: 0;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,39 +1,44 @@
 <template>
-    <header>
-        <h1 v-on:click="changeTitle">{{ title }}</h1>
-    </header>
+  <nav>
+      <ul>
+          <li>
+              <router-link to="/" exact>Blog</router-link>
+          </li>
+          <li>
+              <router-link to="/add" exact>Add Blog</router-link>
+          </li>
+      </ul>
+  </nav>
 </template>
 
 <script>
-import { bus } from "../main";
-
 export default {
-    props: {
-        title: {
-            type: String,
-            required: true,
-        },
-    },
-    data() {
-        return {};
-    },
-    methods: {
-        changeTitle: function () {
-            // this.$emit('changeTitle', 'Vue Ninjas');
-            this.title = "Vue Ninjas";
-            bus.$emit("titleChanged", "Vue Ninjas");
-        },
-    },
-};
+
+}
 </script>
 
 <style scoped>
-header {
-    background: lightgreen;
-    padding: 10px;
-}
-h1 {
-    color: #222;
+ul{
+    list-style-type: none;
     text-align: center;
+    margin: 0;
+}
+li{
+    display: inline-block;
+    margin: 0 10px;
+}
+a{
+    color: #fff;
+    text-decoration: none;
+    padding: 6px 8px;
+}
+nav{
+    background: #444;
+    padding: 14px 0;
+    margin-bottom: 40px;
+}
+.router-link-active{
+    background: #eee;
+    color: #444;
 }
 </style>
